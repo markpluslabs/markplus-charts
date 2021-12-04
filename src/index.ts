5008510c2e182115fe38977354c5dcd11be9c822
Tyler Long <tyler4long@gmail.com>
2021-12-03 23:05:35 -0800

    First working version

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('Hello world!');
+import ELK from 'elkjs';
+
+const elk = new ELK();
+
+const graph = {
+  id: 'root',
+  layoutOptions: {'elk.algorithm': 'layered'},
+  children: [
+    {id: 'n1', width: 30, height: 30},
+    {id: 'n2', width: 30, height: 30},
+    {id: 'n3', width: 30, height: 30},
+  ],
+  edges: [
+    {id: 'e1', sources: ['n1'], targets: ['n2']},
+    {id: 'e2', sources: ['n1'], targets: ['n3']},
+  ],
+};
+
+(async () => {
+  const layout = await elk.layout(graph);
+  console.log(JSON.stringify(layout, null, 2));
+})();
