c217b6e692d93d2f041474b4f45629dee08fa3c7
Tyler Liu <tyler4long@gmail.com>
2024-10-27 12:22:52 -0700

    A complex poc

--- test/chevrotain/lexer/poc.spec.ts
+++ test/chevrotain/lexer/poc.spec.ts
@@ -6,43 +6,72 @@ const WhiteSpace = createToken({
   pattern: /\s+/,
   group: Lexer.SKIPPED,
 });
-const LCurly = createToken({ name: 'LCurly', pattern: /\{/ });
+
+export const Node = createToken({
+  name: 'Node',
+  pattern: /[a-zA-Z0-9_]+/,
+});
+
+const LCurly = createToken({
+  name: 'LCurly',
+  pattern: /\{/,
+  push_mode: 'props_mode',
+});
+
+export const Link = createToken({
+  name: 'Link',
+  pattern: /-->/,
+});
+
 export const PropKey = createToken({
   name: 'PropKey',
   pattern: /[a-zA-Z][a-zA-Z0-9-]*/,
 });
+
 const Colon = createToken({
   name: 'Colon',
   pattern: /:/,
   push_mode: 'value_mode',
 });
+
 const PropValue = createToken({
   name: 'PropValue',
   pattern: /(?:[^};\\]|\\.)+/,
   pop_mode: true,
 });
-const Semicolon = createToken({ name: 'Semicolon', pattern: /;/ });
-const RCurly = createToken({ name: 'RCurly', pattern: /\}/ });
+
+const Semicolon = createToken({
+  name: 'Semicolon',
+  pattern: /;/,
+});
+
+const RCurly = createToken({
+  name: 'RCurly',
+  pattern: /\}/,
+  pop_mode: true,
+});
 
 export const multiModeLexerDefinition = {
   modes: {
-    props_mode: [WhiteSpace, LCurly, PropKey, Colon, Semicolon, RCurly],
+    statement_mode: [WhiteSpace, Node, LCurly, Link],
+    props_mode: [WhiteSpace, PropKey, Colon, Semicolon, RCurly],
     value_mode: [WhiteSpace, PropValue],
   },
-  defaultMode: 'props_mode',
+  defaultMode: 'statement_mode',
 };
 
 const lexer = new Lexer(multiModeLexerDefinition);
 
 describe('poc lexer', () => {
   test('simple', () => {
-    const r = lexer.tokenize('{prop3: c; prop4: d}');
+    const r = lexer.tokenize('A{prop3: c; prop4: d} -->{prop1: 4} B{prop2: 5}');
     if (r.errors.length > 0) {
       console.log(r.errors);
     }
     expect(r.errors.length).toBe(0);
     const tokens = r.tokens.map((t) => `${t.tokenType.name}: ${t.image}`);
     expect(tokens).toEqual([
+      'Node: A',
       'LCurly: {',
       'PropKey: prop3',
       'Colon: :',
@@ -51,6 +80,18 @@ describe('poc lexer', () => {
       'PropKey: prop4',
       'Colon: :',
       'PropValue: d',
+      'RCurly: }',
+      'Link: -->',
+      'LCurly: {',
+      'PropKey: prop1',
+      'Colon: :',
+      'PropValue: 4',
+      'RCurly: }',
+      'Node: B',
+      'LCurly: {',
+      'PropKey: prop2',
+      'Colon: :',
+      'PropValue: 5',
       'RCurly: }',
     ]);
   });
