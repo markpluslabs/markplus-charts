3dd3069f7b337c82d7358e433b01d8ee7c7d3f9e
Tyler Liu <tyler4long@gmail.com>
2024-10-29 10:07:16 -0700

    Add tests for shapes

---
+++ test/svg/shapes.spec.ts
@@ -0,0 +1,30 @@
+import { describe, expect, test } from 'vitest';
+
+import { generate } from '../../src';
+
+describe('shapes', () => {
+  test('rect', async () => {
+    const svgStr = await generate('A');
+    expect(svgStr).toContain('<rect');
+  });
+
+  test('rect 2', async () => {
+    const svgStr = await generate('A{shape:rect}');
+    expect(svgStr).toContain('<rect');
+  });
+
+  test('circle', async () => {
+    const svgStr = await generate('A{shape:circle}');
+    expect(svgStr).toContain('<circle');
+  });
+
+  test('circle', async () => {
+    const svgStr = await generate('A{shape:circle}');
+    expect(svgStr).toContain('<circle');
+  });
+
+  test('ellipse', async () => {
+    const svgStr = await generate('A{shape:ellipse}');
+    expect(svgStr).toContain('<ellipse');
+  });
+});
