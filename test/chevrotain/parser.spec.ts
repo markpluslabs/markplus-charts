48b74281d49215017cb20642e29dbb80bd473927
Tyler Liu <tyler4long@gmail.com>
2025-01-16 11:40:23 -0800

    Format code

--- test/chevrotain/parser.spec.ts
+++ test/chevrotain/parser.spec.ts
@@ -1,11 +1,11 @@
-import { describe, expect, test } from 'vitest';
+import { describe, expect, test } from "vitest";
 
-import lexer from '../../src/chevrotain/lexer';
-import parser from '../../src/chevrotain/parser';
+import lexer from "../../src/chevrotain/lexer";
+import parser from "../../src/chevrotain/parser";
 
-describe('parser', () => {
-  test('default', () => {
-    const input = 'A{prop3: c; prop4: d} ->{prop1: 4} B{prop2: 5}';
+describe("parser", () => {
+  test("default", () => {
+    const input = "A{prop3: c; prop4: d} ->{prop1: 4} B{prop2: 5}";
     const r = lexer.tokenize(input);
     parser.input = r.tokens;
     const cst = parser.parse();
--- test/chevrotain/visitor.spec.ts
+++ test/chevrotain/visitor.spec.ts
@@ -1,81 +1,81 @@
-import { describe, expect, test } from 'vitest';
+import { describe, expect, test } from "vitest";
 
-import { generateAst } from '../../src/chevrotain';
+import { generateAst } from "../../src/chevrotain";
 
-describe('visitor', () => {
-  test('default', () => {
-    const input = 'A{prop3: c; prop4: d} ->{prop1: 4} B{prop2: 5}';
+describe("visitor", () => {
+  test("default", () => {
+    const input = "A{prop3: c; prop4: d} ->{prop1: 4} B{prop2: 5}";
     const ast = generateAst(input);
     expect(ast.plainObject).toEqual({
       props: {},
       nodes: [
-        { id: 'A', props: { prop3: 'c', prop4: 'd' } },
-        { id: 'B', props: { prop2: '5' } },
+        { id: "A", props: { prop3: "c", prop4: "d" } },
+        { id: "B", props: { prop2: "5" } },
       ],
-      links: [{ id: 'A-B', from: 'A', to: 'B', props: { prop1: '4' } }],
+      links: [{ id: "A-B", from: "A", to: "B", props: { prop1: "4" } }],
     });
   });
-  test('single node', () => {
-    const ast = generateAst('A');
+  test("single node", () => {
+    const ast = generateAst("A");
     expect(ast.plainObject).toEqual({
       props: {},
-      nodes: [{ id: 'A', props: {} }],
+      nodes: [{ id: "A", props: {} }],
       links: [],
     });
   });
 
-  test('single node with props', () => {
-    const ast = generateAst('A{b: c}');
+  test("single node with props", () => {
+    const ast = generateAst("A{b: c}");
     expect(ast.plainObject).toEqual({
       props: {},
-      nodes: [{ id: 'A', props: { b: 'c' } }],
+      nodes: [{ id: "A", props: { b: "c" } }],
       links: [],
     });
   });
 
-  test('trailing space in propValue', () => {
-    const ast = generateAst('A{b: c }');
+  test("trailing space in propValue", () => {
+    const ast = generateAst("A{b: c }");
     expect(ast.plainObject).toEqual({
       props: {},
-      nodes: [{ id: 'A', props: { b: 'c' } }],
+      nodes: [{ id: "A", props: { b: "c" } }],
       links: [],
     });
   });
 
-  test('whole line comments', () => {
+  test("whole line comments", () => {
     const ast = generateAst(`// comment
 A{b: c}
  //comment`);
     expect(ast.plainObject).toEqual({
       props: {},
-      nodes: [{ id: 'A', props: { b: 'c' } }],
+      nodes: [{ id: "A", props: { b: "c" } }],
       links: [],
     });
   });
 
-  test('line end comments', () => {
+  test("line end comments", () => {
     const ast = generateAst(`A{b: c} // comment`);
     expect(ast.plainObject).toEqual({
       props: {},
-      nodes: [{ id: 'A', props: { b: 'c' } }],
+      nodes: [{ id: "A", props: { b: "c" } }],
       links: [],
     });
   });
 
-  test('escape comments', () => {
+  test("escape comments", () => {
     const ast = generateAst(`A{b: https:\\//www.google.com}`);
     expect(ast.plainObject).toEqual({
       props: {},
-      nodes: [{ id: 'A', props: { b: 'https://www.google.com' } }],
+      nodes: [{ id: "A", props: { b: "https://www.google.com" } }],
       links: [],
     });
   });
 
-  test('global props', () => {
+  test("global props", () => {
     const ast = generateAst(`direction: up\nA{b: https:\\//www.google.com}`);
     expect(ast.plainObject).toEqual({
-      props: { direction: 'up' },
-      nodes: [{ id: 'A', props: { b: 'https://www.google.com' } }],
+      props: { direction: "up" },
+      nodes: [{ id: "A", props: { b: "https://www.google.com" } }],
       links: [],
     });
   });
